8edd7b7543f20a8e0ba7b557e67c6431e450979a
Nik Erlandsson <nik.erlandsson@gmail.com>
2026-04-18 22:18:51 +0100

    Add argument for highest element value

--- sw/quadrisparse_spmm/gen_mat.py
+++ sw/quadrisparse_spmm/gen_mat.py
@@ -2,40 +2,43 @@ import sys
 import random
 import numpy as np
 
-MAX_VAL = 15
 
 def generate_spmm_test_data(*args): 
-    if len(args) != 2:
-        raise ValueError("Expected 2 arguments: Size, Sparsity")
+    if len(args) < 2:
+        raise ValueError("Expected 3 arguments: Size, Sparsity, MaxVal")
+    if len(args) == 2:
+        MAX_VAL = 15
+    else:
+        MAX_VAL = int(args[2])
     
-    size = int(args[0])
-    sparsity = float(args[1])
+    SIZE = int(args[0])
+    SPARSITY = float(args[1])
+
+    print(f"Generating SpMM test data with size {SIZE} and sparsity {SPARSITY}")
     
-    print(f"Generating SpMM test data with size {size} and sparsity {sparsity}")
-    
-    dense_matrix = np.zeros((size, size), dtype=np.int32)
-    sparse_matrix = np.zeros((size, size), dtype=np.int32)
+    dense_matrix = np.zeros((SIZE, SIZE), dtype=np.int32)
+    sparse_matrix = np.zeros((SIZE, SIZE), dtype=np.int32)
     
     # Dense matrix
-    with open(f"mat_{size}_{sparsity}_b.hex", "w") as f_dense:
-        for i in range(size):
-            for j in range(size):
+    with open(f"mat_{SIZE}_{SPARSITY}_b.hex", "w") as f_dense:
+        for i in range(SIZE):
+            for j in range(SIZE):
                 val = random.randint(0, MAX_VAL)
                 dense_matrix[i][j] = val
                 f_dense.write(f"{val:08x}\n")
                 
             
     # Sparse matrix in CSR format
-    with open(f"mat_{size}_{sparsity}_a_val.hex", "w") as f_val, \
-         open(f"mat_{size}_{sparsity}_a_col.hex", "w") as f_col, \
-         open(f"mat_{size}_{sparsity}_a_row.hex", "w") as f_row:
+    with open(f"mat_{SIZE}_{SPARSITY}_a_val.hex", "w") as f_val, \
+         open(f"mat_{SIZE}_{SPARSITY}_a_col.hex", "w") as f_col, \
+         open(f"mat_{SIZE}_{SPARSITY}_a_row.hex", "w") as f_row:
 
         row_ptr = 0
         f_row.write(f"{row_ptr:08x}\n")
-        for i in range(size):
+        for i in range(SIZE):
             nnz = 0
-            for j in range(size):
-                if random.random() > sparsity:
+            for j in range(SIZE):
+                if random.random() > SPARSITY:
                     nnz += 1
                     val = random.randint(0, MAX_VAL)
                     sparse_matrix[i][j] = val
@@ -48,9 +51,9 @@ def generate_spmm_test_data(*args):
             
     
     result = np.dot(sparse_matrix, dense_matrix)
-    with open(f"mat_{size}_{sparsity}_ref.hex", "w") as f_result:
-        for i in range(size):
-            for j in range(size):
+    with open(f"mat_{SIZE}_{SPARSITY}_ref.hex", "w") as f_result:
+        for i in range(SIZE):
+            for j in range(SIZE):
                 f_result.write(f"{result[i][j]:08x}\n")
     
     print("\n######## Dense matrix ########")
